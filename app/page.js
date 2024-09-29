270c4feecbc8ce73f900cde1a77b886fd43fe3b0
Raghav Pillai <raghavpillai101@gmail.com>
2024-09-29 16:43:50 -0700

    Removed Navbar component and related imports
    - Removed Navbar component from app/page.js
    - Removed related imports from app/sections/navbar.js

--- app/page.js
+++ app/page.js
@@ -5,12 +5,10 @@ import { Flex } from "@chakra-ui/react";
 import About from "./sections/about/about";
 import Footer from "./sections/footer";
 import Hero from "./sections/hero";
-import Navbar from "./sections/navbar";
 
 export default function Home() {
   return (
     <Flex direction="column" minH="100vh">
-      <Navbar />
       <Hero />
       <About />
       <Footer />
